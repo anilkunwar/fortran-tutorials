e380c86c18f5dd729bf37be0c5a773a4cbba9939
Anil Kunwar <anilkunwar318@yahoo.com>
2017-03-31 00:33:50 +0800

    Update cuweighttomolefraction.F90

--- thermophysical_properties/CuSolubilityinMoltenSn/cuweighttomolefraction.F90
+++ thermophysical_properties/CuSolubilityinMoltenSn/cuweighttomolefraction.F90
@@ -5,15 +5,19 @@
 !CuWftoXf.F90 and cu_fraction_mass_to_molefraction.F90
 program cusolubilityxf
 implicit none
-real,parameter::mcu=63.546 !molar weight of Cu in grams
-real,parameter::msn=118.710 !molar weight of Sn in grams
+real,parameter::mcu=63.546 !molar weight of Cu in grams/mole
+real,parameter::msn=118.710 !molar weight of Sn in grams/mole
 real::weightfractionofcu     !weight fraction of Cu in decimals
 real::molefractionofcu  !mole fraction of Cu in decimals
+real::moleofcu  !weight fraction of Cu divided by its molar weight
+real:moleofsn   !weight fraction of Sn divided by its molar weight
 !real::bd  !bubble diameter
 write(*,*)'write the weight fraction of Cu in fraction or decimal (0.1307 at 415 degree centigrade)'
 read(*,*)weightfractionofcu 
+moleofcu=weightfractionofcu/mcu
+moleofsn=(1-weightfractionofcu)/msn
 !write(*,*)'write the value of measured diamter at surface(millimeters):'
 !read(*,*)measured
-molefractionofcu=(weightfractionofcu*mcu)/(mcu + msn)
+molefractionofcu=(weightfractionofcu/mcu)*(moleofcu + moleofsn)
 write(*,*)'The mole fraction of Cu in Sn-Cu mixture is',molefractionofcu 
 end program cusolubilityxf
