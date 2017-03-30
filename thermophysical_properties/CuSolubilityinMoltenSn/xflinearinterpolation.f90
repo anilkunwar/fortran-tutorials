aa8df698677101dd5b8d8add532235b93a1f17f3
Anil Kunwar <anilkunwar318@yahoo.com>
2017-03-31 01:16:23 +0800

    Update xflinearinterpolation.f90

--- thermophysical_properties/CuSolubilityinMoltenSn/xflinearinterpolation.f90
+++ thermophysical_properties/CuSolubilityinMoltenSn/xflinearinterpolation.f90
@@ -1,33 +1,35 @@
-PROGRAM Diffusivity
-!This program is used for the calculation of arrhenious diffusivity  of a given Cu material in liquid Sn
-! D_{T} = D_{o}*exp{-Q_{a}/RT} , where D_{o} = 3.66*e-7 and Q_{a} = 17,556  J/mol  A. Kunwar et al., J. Mater. Sci.: Mater. Electron.(2016) 27: 7699–7706 
+PROGRAM Cumolarity
+!This program fits the molarity of Cu in Sn as a linear function of temperature T
+! x_f{T} = x_f{1} + slope*(T-T_l} , where x_f{1} = molarity at T_l and slope = \frac{x_f{2}-x_f{1}}.{T_h - T_l}
 IMPLICIT NONE
-Real, Parameter::R=8.31 !Universal Gas Constant
-Real, Parameter::e=2.718282 !value of napiers constant
 Real::T !Temperature in Kelvin
-Real::Earrh !Exponential constant in Arrhenius Equation i.e. Q_a
-Real::A !Amplification Factor or Constant D_o
-Real::diffcoeff !diffusivity
-Real :: x 
-Real :: y 
+Real,Parameter:: Tl= 499.95 !lower temperature equal to 226.8 degree centigrade
+Real,Parameter:: Th= 688.15 !Higher temperature equal to 415 degree centigrade
+Real,Parameter:: xl= 0.001649 !molarity of Cu corresponding to lower temperature equal to 226.8 degree centigrade
+Real,Parameter:: xh= 0.21928 !molarity of Cu corresponding to Higher temperature equal to 415 degree centigrade
+Real::xft !molarity at temperature T
+Real :: slope
 !Real:: A, B
+slope = (xh-xl)/(Th-Tl)
 Integer :: Counter
 Write(*,*)'starting T (in Kelvin Scale):'
 Read(*,*)T
-Write(*,*)'A (eg: A for Cu in Sn = 3.66e-7):'
-Read(*,*) A
-Write(*,*)'Earrh(eg: Earrh for Cu in tin= 17556):'
-Read(*,*)Earrh
+!Write(*,*)'A (eg: A for Cu in Sn = 3.66e-7):'
+!Read(*,*) A
+!@Write(*,*)'Earrh(eg: Earrh for Cu in tin= 17556):'
+!Read(*,*)Earrh
 !T=T+273.15 !Convert to Kelvin Scale
-Print*,"#Temperature (K) Diffusivity(kg m^(-3))"
-DO Counter = 1, 109, 1
+Print*,"#Temperature(K) Molarity"
+DO Counter = 1, 209, 1
 T = T + 1.0
-x= Earrh/(R*T)
-y=e**(-x)
-diffcoeff=A*y
+!x= Earrh/(R*T)
+!y=e**(-x)
+!diffcoeff=A*y
 !Write(*,*)'T (K):',T
 !Write(*,*)'The Viscosity(mPa.S):',visc
 !rho = A + B * T
-Write(*,*)T,diffcoeff
+xft = xl + slope * (T - Tl)
+!Write(*,*)'Interpolated value of IMC diameter:', y 
+Write(*,*)T,xft
 END DO
-END PROGRAM Diffusivity
+END PROGRAM Cumolarity
