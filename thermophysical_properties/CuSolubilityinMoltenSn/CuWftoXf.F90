cdb0c31add07c062c7131cbb5335e9a3e0a86ea2
Anil Kunwar <anilkunwar318@yahoo.com>
2017-03-31 00:04:18 +0800

    Create CuWftoXf.F90

---
+++ thermophysical_properties/CuSolubilityinMoltenSn/CuWftoXf.F90
@@ -0,0 +1,18 @@
+! The data for this calculation are taken from http://www.metallurgy.nist.gov/phase/solder/cusn.html
+! M.L. Huang et al, APPLIED PHYSICS LETTERS 86, 181908 (2005)http://aip.scitation.org/doi/pdf/10.1063/1.1925317
+!This program calculates the mole fraction of Cu from the mass fraction in Cu-Sn mixture
+!x_f=(w_f*M_Cu)/(M_Cu + M_Sn)
+program cusolubilityxf
+implicit none
+real,parameter::mcu=63.5 !molar weight of Cu in grams
+real,parameter::msn=118 !molar weight of Sn in grams
+real::weightfractionofcu     !weight fraction of Cu in decimals
+real::molefractionofcu  !mole fraction of Cu in decimals
+!real::bd  !bubble diameter
+write(*,*)'write the weight fraction of Cu in fraction or decimal (0.1307 at 415 degree centigrade)'
+read(*,*)weightfractionofcu 
+!write(*,*)'write the value of measured diamter at surface(millimeters):'
+!read(*,*)measured
+molefractionofcu=(weightfractionofcu*mcu)/d(mcu + msn)
+write(*,*)'The mole fraction of Cu in Sn-Cu mixture is',molefractionofcu 
+end program cusolubilityxf
